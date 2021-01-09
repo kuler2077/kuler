785d3681edd49be79153dd88fe30bcfbeb1db5ab
kuler <jjzsk@qq.com>
2021-01-09 17:03:02 +0800

    add some code

---
+++ src/me/number/Study.java
@@ -0,0 +1,14 @@
+package me.number;
+
+public class Study {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println("i = " + i);
+            }
+
+        }
+    }
+
+}
+
---
+++ src/me/number/Study03.java
@@ -0,0 +1,5 @@
+package me.number;
+
+public class Study03 {
+
+}
